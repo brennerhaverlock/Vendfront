4fc4d91de3fb855f6e91cc5f0f8919f7be63fa5f
Raju Ahammad <rajuahammad73@gmail.com>
2021-10-14 10:12:59 +0600

    home 9 set as root

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,85 +1,46 @@
 import Head from "next/head";
-import {getBlogPosts} from "@utils/blog";
-import LatestBlog from "@components/blog";
-import {Fragment, useContext} from "react";
-import {getProductsBySkin} from "@utils/product";
-import sliderData from "@data/slider/home-one.json";
-import {HeaderOne as Header} from "@components/header";
-import {SliderOne as Slider} from "@components/slider";
-import {FooterOne as Footer} from "@components/footer";
-import {ProductsContext} from "@global/ProductsContext";
-import {HomePagesNavData as navContent} from "@data/navbar";
-import {ServicesOne as Services} from "@components/services";
-import {CategoriesOne as Categories} from "@components/categories";
-import {ContentWrapperOne as ContentWrapper} from "@components/wrapper";
-import {PromoBannerOne as PromoBanners} from "@components/promo-banners";
-import {BestSelling, TendingProducts as Tending} from "@components/products";
+import { Fragment, useContext } from "react";
+import { getProductsBySkin } from "@utils/product";
+import { AboutTwo as About } from "@components/about";
+import { HeaderSix as Header } from "@components/header";
+import { ProductsContext } from "@global/ProductsContext";
+import { BrandsThree as Brands } from "@components/brands";
+import { FooterThree as Footer } from "@components/footer";
+import { HomePagesNavData as navContent } from "@data/navbar";
+import { CategoriesSix as Categories } from "@components/categories";
+import { HomeSixProductsTab as Products } from "@components/products";
+import { TestimonialOne as Testimonials } from "@components/testimonials";
+import { ContentWrapperOne as ContentWrapper } from "@components/wrapper";
 
-const Home = ({blogs}) => {
-    const {products} = useContext(ProductsContext);
-    const productsFashion = getProductsBySkin(products, "fashion");
-    const logo = "/assets/images/no-placeholder/logo.png";
+const Home = () => {
+  const { products } = useContext(ProductsContext);
+  const productsFashion = getProductsBySkin(products, "fashion");
+  const logo = "/assets/images/no-placeholder/logo.png";
 
-    return (
-        <Fragment>
-            <Head>
-                <title>[Your Store]</title>
-            </Head>
+  return (
+    <Fragment>
+      <Head>
+        <title>Home - [Store Name]</title>
+      </Head>
 
-            <Header
-                logo={logo}
-                navbar={true}
-                dark={false}
-                navData={navContent}
-                navbarAlignment="left"
-            />
+      <Header
+        logo={logo}
+        navData={navContent}
+        containerFluid={false}
+        logoAlignment="center"
+        navbarAlignment="center"
+      />
 
-            <ContentWrapper>
-                <Slider
-                    dots={true}
-                    arrows={true}
-                    data={sliderData}
-                    className="nomargin"
-                />
-                <Categories/>
-                <Tending
-                    products={productsFashion}
-                />
-                <PromoBanners/>
-                <BestSelling
-                    products={productsFashion}
-                />
-                <LatestBlog
-                    blogs={blogs}
-                />
-                <Services/>
-            </ContentWrapper>
+      <ContentWrapper>
+        <Categories className="nomargin" />
+        <About />
+        <Products products={productsFashion} />
+        <Testimonials />
+        <Brands />
+      </ContentWrapper>
 
-            <Footer
-                logo={logo}
-                dark={false}
-                newsletter={true}
-            />
-        </Fragment>
-    )
-}
-
-export async function getStaticProps() {
-    const blogs = getBlogPosts([
-        'title',
-        'excerpt',
-        'date',
-        'author',
-        'thumb',
-        'slug',
-        'categories'
-    ], 3);
-
-    return {
-        props: {
-            blogs: blogs
-        }
-    }
-}
-
+      <Footer logo={logo} dark={false} newsletter={false} />
+    </Fragment>
+  );
+};
 export default Home;
