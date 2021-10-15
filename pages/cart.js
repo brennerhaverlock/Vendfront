36c0e40f76f474a419eaa50c357de5ad9ab1a7b1
Brenner Haverlock <80720533+officialbrenner@users.noreply.github.com>
2021-10-15 10:33:55 -0700

    dynamic cart view added

--- pages/cart.js
+++ pages/cart.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import Layout from '../components/Layout';
 import { Store } from '../utils/Store';
+import dynamic from 'next/dynamic';
 import NextLink from 'next/link';
 import Image from 'next/image';
 import {
@@ -21,7 +22,7 @@ import {
   ListItem,
 } from '@material-ui/core';
 
-export default function CartScreen() {
+ function CartScreen() {
   const { state } = useContext(Store);
   const {
     cart: { cartItems },
@@ -117,3 +118,5 @@ export default function CartScreen() {
     </Layout>
   );
 }
+
+export default dynamic(()=> Promise.resolve(CartScreen), {ssr: false})
