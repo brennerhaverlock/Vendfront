306386681d18fa9c25f9a4119532c024d58719e8
Brenner Haverlock <80720533+officialbrenner@users.noreply.github.com>
2021-10-16 06:40:21 -0700

    setup dir for local

--- pages/index.js
+++ pages/index.js
@@ -10,34 +10,15 @@ import {
 } from '@material-ui/core';
 import NextLink from 'next/link';
 import Layout from '../components/Layout';
-import db from '../utils/db';
-import Product from '../models/Product';
-import axios from 'axios';
-import { useRouter } from 'next/router';
-import { useContext } from 'react';
-import { Store } from '../utils/Store';
+import data from '../utils/data';
 
-export default function Home(props) {
-  const router = useRouter();
-  const { state, dispatch } = useContext(Store);
-  const { products } = props;
-  const addToCartHandler = async (product) => {
-    const existItem = state.cart.cartItems.find((x) => x._id === product._id);
-    const quantity = existItem ? existItem.quantity + 1 : 1;
-    const { data } = await axios.get(`/api/products/${product._id}`);
-    if (data.countInStock < quantity) {
-      window.alert('Sorry. Product is out of stock');
-      return;
-    }
-    dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } });
-    router.push('/cart');
-  };
+export default function Home() {
   return (
     <Layout>
       <div>
         <h1>Products</h1>
         <Grid container spacing={3}>
-          {products.map((product) => (
+          {data.products.map((product) => (
             <Grid item md={4} key={product.name}>
               <Card>
                 <NextLink href={`/product/${product.slug}`} passHref>
@@ -54,11 +35,7 @@ export default function Home(props) {
                 </NextLink>
                 <CardActions>
                   <Typography>${product.price}</Typography>
-                  <Button
-                    size="small"
-                    color="primary"
-                    onClick={() => addToCartHandler(product)}
-                  >
+                  <Button size="small" color="primary">
                     Add to cart
                   </Button>
                 </CardActions>
@@ -69,15 +46,4 @@ export default function Home(props) {
       </div>
     </Layout>
   );
-}
-
-export async function getServerSideProps() {
-  await db.connect();
-  const products = await Product.find({}).lean();
-  await db.disconnect();
-  return {
-    props: {
-      products: products.map(db.convertDocToObj),
-    },
-  };
 }
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import NextLink from 'next/link';
 import Image from 'next/image';
 import {
@@ -10,34 +10,19 @@ import {
   Card,
   Button,
 } from '@material-ui/core';
+import { useRouter } from 'next/router';
+import data from '../../utils/data';
 import Layout from '../../components/Layout';
 import useStyles from '../../utils/styles';
-import Product from '../../models/Product';
-import db from '../../utils/db';
-import axios from 'axios';
-import { Store } from '../../utils/Store';
-import { useRouter } from 'next/router';
 
-export default function ProductScreen(props) {
-  const router = useRouter();
-  const { state, dispatch } = useContext(Store);
-  const { product } = props;
+export default function ProductScreen() {
   const classes = useStyles();
+  const router = useRouter();
+  const { slug } = router.query;
+  const product = data.products.find((a) => a.slug === slug);
   if (!product) {
     return <div>Product Not Found</div>;
   }
-  const addToCartHandler = async () => {
-    const existItem = state.cart.cartItems.find((x) => x._id === product._id);
-    const quantity = existItem ? existItem.quantity + 1 : 1;
-    const { data } = await axios.get(`/api/products/${product._id}`);
-    if (data.countInStock < quantity) {
-      window.alert('Sorry. Product is out of stock');
-      return;
-    }
-    dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } });
-    router.push('/cart');
-  };
-
   return (
     <Layout title={product.name} description={product.description}>
       <div className={classes.section}>
@@ -106,12 +91,7 @@ export default function ProductScreen(props) {
                 </Grid>
               </ListItem>
               <ListItem>
-                <Button
-                  fullWidth
-                  variant="contained"
-                  color="primary"
-                  onClick={addToCartHandler}
-                >
+                <Button fullWidth variant="contained" color="primary">
                   Add to cart
                 </Button>
               </ListItem>
@@ -121,18 +101,4 @@ export default function ProductScreen(props) {
       </Grid>
     </Layout>
   );
-}
-
-export async function getServerSideProps(context) {
-  const { params } = context;
-  const { slug } = params;
-
-  await db.connect();
-  const product = await Product.findOne({ slug }).lean();
-  await db.disconnect();
-  return {
-    props: {
-      product: db.convertDocToObj(product),
-    },
-  };
 }
