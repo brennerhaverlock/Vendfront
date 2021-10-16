ac62f2d43881ddbb893be052e20dab587db8ceb8
Brenner Haverlock <80720533+officialbrenner@users.noreply.github.com>
2021-10-16 07:11:30 -0700

    import and change for local env

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -1,14 +1,62 @@
-mport nc from 'next-connect';
-import Product from '../../../models/Product';
-import db from '../../../utils/db';
+import {
+  Grid,
+  Card,
+  CardActionArea,
+  CardMedia,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+} from '@material-ui/core';
+import NextLink from 'next/link';
+import Layout from '../components/Layout';
+import db from '../utils/db';
+import Product from '../models/Product';
 
-const handler = nc();
+export default function Home(props) {
+  const { products } = props;
+  return (
+    <Layout>
+      <div>
+        <h1>Products</h1>
+        <Grid container spacing={3}>
+          {products.map((product) => (
+            <Grid item md={4} key={product.name}>
+              <Card>
+                <NextLink href={`/product/${product.slug}`} passHref>
+                  <CardActionArea>
+                    <CardMedia
+                      component="img"
+                      image={product.image}
+                      title={product.name}
+                    ></CardMedia>
+                    <CardContent>
+                      <Typography>{product.name}</Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </NextLink>
+                <CardActions>
+                  <Typography>${product.price}</Typography>
+                  <Button size="small" color="primary">
+                    Add to cart
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </div>
+    </Layout>
+  );
+}
 
-handler.get(async (req, res) => {
+export async function getServerSideProps() {
   await db.connect();
-  const products = await Product.find({});
+  const products = await Product.find({}).lean();
   await db.disconnect();
-  res.send(products);
-});
-
-export default handler;
+  return {
+    props: {
+      products: products.map(db.convertDocToObj),
+    },
+  };
+}
