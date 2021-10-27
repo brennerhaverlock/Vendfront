51f9c038172f6addda1a1e71a7051dc0e0792bfb
Brenner Haverlock <80720533+officialbrenner@users.noreply.github.com>
2021-10-27 13:17:30 -0700

    init about page

---
+++ pages/about.js
@@ -0,0 +1,21 @@
+import {
+    Grid,
+    TableContainer,
+    Table,
+    Typography,
+    TableHead,
+    TableBody,
+    TableRow,
+    TableCell,
+    Link,
+    Select,
+    MenuItem,
+    Button,
+    Card,
+    List,
+    ListItem,
+  } from '@material-ui/core';
+  import axios from 'axios';
+  import { useRouter } from 'next/router';
+
+  
